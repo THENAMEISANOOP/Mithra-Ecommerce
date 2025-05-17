6c2528779415b77ffb83a323f71cb6dd9f692857
THENAMEISANOOP <sanoopanoop444@gmail.com>
2025-05-17 23:54:37 +0530

    Navbar Complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,31 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Collection from "./pages/Collection"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Product from "./pages/Product"
+import Cart from "./pages/Cart"
+import Login from "./pages/Login"
+import PlaceOrder from "./pages/PlaceOrder"
+import Orders from "./pages/Orders"
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
-    <div className="bg-green-500 h-screen flex items-center justify-center">
-      <h1 className="text-red-500 text-3xl font-bold ">Hello Tailwind!</h1>
+    <div className="px-4 sm:px-[5vw]   md:px-[vw] lg:px-[9vw] ">
+      <Navbar/>
+      <Routes>
+        <Route  path="/" element={<Home/>}/>
+        <Route path="/collection" element={<Collection/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/product/productId" element={<Product/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/place-order" element={<PlaceOrder/>}/>
+        <Route path="/orders" element={<Orders/>}/>
+      </Routes>
+
     </div>
   )
 }
