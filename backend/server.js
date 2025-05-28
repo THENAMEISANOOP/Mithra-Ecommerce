8dc4a636432038fe6f43d72506fa9ac19ed58b78
THENAMEISANOOP <sanoopanoop444@gmail.com>
2025-05-28 18:18:20 +0530

    backend setup complte

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+// app configuration
+const app = express();
+const PORT = process.env.PORT || 5001;
+connectDB();   
+connectCloudinary();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// api endpoints
+app.get('/', (req, res) => {
+  res.send('api is running');
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
